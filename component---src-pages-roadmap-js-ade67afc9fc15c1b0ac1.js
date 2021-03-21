c4c8ad56176179fb7653062eacd6098f4cb45fe4
Snowge McFloofy <snowgecoin@gmail.com>
2021-03-20 21:51:52 -0500

    forgot to commit public dir

---
+++ component---src-pages-roadmap-js-ade67afc9fc15c1b0ac1.js
@@ -0,0 +1,2 @@
+(self.webpackChunksnowge=self.webpackChunksnowge||[]).push([[917],{3667:function(e,n,l){"use strict";l.r(n);var a=l(7294),c=l(8621),s=l(2342);n.default=function(){return a.createElement("div",{className:"app-background flex-column scrollbar"},a.createElement(c.Z,null),a.createElement("div",{className:"flex-row x-centre"},"Coming soon! I promis."),a.createElement("span",{className:"flex-spacer"}),a.createElement(s.Z,null))}}}]);
+//# sourceMappingURL=component---src-pages-roadmap-js-ade67afc9fc15c1b0ac1.js.map
