47f331734e30dbb834d03c50fa6a81b054b218bc
Snowge McFloofy <snowgecoin@gmail.com>
2021-03-20 19:05:41 -0500

    i know i should commit to master, I'll do that in a sec

---
+++ component---src-pages-floofypaper-js-dbd2c5f2889909658841.js
@@ -0,0 +1,2 @@
+(self.webpackChunksnowge=self.webpackChunksnowge||[]).push([[25],{1755:function(e,l,n){"use strict";n.r(l);var a=n(7294),c=n(8621),s=n(2342);l.default=function(){return a.createElement("div",{className:"app-background flex-column scrollbar"},a.createElement(c.Z,null),a.createElement("span",{className:"flex-spacer"}),a.createElement(s.Z,null))}}}]);
+//# sourceMappingURL=component---src-pages-floofypaper-js-dbd2c5f2889909658841.js.map
