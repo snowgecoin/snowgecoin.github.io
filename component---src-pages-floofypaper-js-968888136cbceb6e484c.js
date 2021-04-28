208c6ac95266713c349759585ce6c2a960600ca5
Snowge McFloofy <snowgecoin@gmail.com>
2021-04-27 23:03:41 -0500

    Basic web3 integration

---
+++ component---src-pages-floofypaper-js-968888136cbceb6e484c.js
@@ -0,0 +1,2 @@
+(self.webpackChunksnowge=self.webpackChunksnowge||[]).push([[25],{1755:function(e,l,n){"use strict";n.r(l);var a=n(7294),c=n(2944),s=n(9255);l.default=function(){return a.createElement("div",{className:"app-background flex-column scrollbar"},a.createElement(c.Z,null),a.createElement("span",{className:"flex-spacer"}),a.createElement(s.Z,null))}}}]);
+//# sourceMappingURL=component---src-pages-floofypaper-js-968888136cbceb6e484c.js.map
