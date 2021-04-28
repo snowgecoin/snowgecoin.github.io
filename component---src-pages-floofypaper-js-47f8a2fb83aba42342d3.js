ea6d3a64927e556201ed10fb6ba6dbf81c64088b
Snowge McFloofy <snowgecoin@gmail.com>
2021-04-28 12:13:24 -0500

    more web3 stuff, kinda

---
+++ component---src-pages-floofypaper-js-47f8a2fb83aba42342d3.js
@@ -0,0 +1,2 @@
+(self.webpackChunksnowge=self.webpackChunksnowge||[]).push([[25],{1755:function(e,l,n){"use strict";n.r(l);var a=n(7294),c=n(4795),s=n(9255);l.default=function(){return a.createElement("div",{className:"app-background flex-column scrollbar"},a.createElement(c.Z,null),a.createElement("span",{className:"flex-spacer"}),a.createElement(s.Z,null))}}}]);
+//# sourceMappingURL=component---src-pages-floofypaper-js-47f8a2fb83aba42342d3.js.map
